779bbb2dea1f051934052caad64a0907b4a65270
joshua-clores-dev <jclores@payremit.com>
2026-02-11 16:41:36 +0800

    learn: explicit types and type inference

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+// My first greetings in TypeScript
 const greetings: string = "Hello, World!";
 console.log(greetings);
 
+//Learning Basic Types
 let isActive: boolean = true;
 let hasPermission: boolean = false;
 console.log(`Is Active: ${isActive}, Has Permission: ${hasPermission}`);
@@ -12,3 +14,15 @@ console.log(`Count: ${count}, Price: $${price}`);
 let fullName: string = "Joshua Clores";
 let email: string = "josh.clores08@gmail.com";
 console.log(`Name: ${fullName}, Email: ${email}`);
+
+//Learning Explicit Types
+let age: number = 25;
+let address: string = "Caloocan City, Philippines";
+let isStudent: boolean = true;
+console.log(`Age: ${age}, Address: ${address}, Is Student: ${isStudent}`);
+
+//Learning Type Inference
+let username = "clo";
+let score = 100;
+let isLoggedIn = false;
+console.log(`Username: ${username}, Score: ${score}, Is Logged In: ${isLoggedIn}`);
